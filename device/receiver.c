6355310d27c15b3c395e73c8652276273e75f721
Dexter M Haslem <git@dmh.fm>
2022-06-12 14:38:06 -0600

    clearer project names

---
+++ device/receiver.c
@@ -0,0 +1,8 @@
+/*
+ * receiver.c
+ *
+ *  Created on: Jun 12, 2022
+ *      Author: dexter
+ */
+
+
---
+++ device/receiver.h
@@ -0,0 +1,13 @@
+/*
+ * receiver.h
+ *
+ *  Created on: Jun 12, 2022
+ *      Author: dexter
+ */
+
+#ifndef RECEIVER_H_
+#define RECEIVER_H_
+
+
+
+#endif /* RECEIVER_H_ */
